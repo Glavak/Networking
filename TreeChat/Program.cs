7cede2d020ac40f9c03925d87fedd5aeeb043fd8
GLAVAK <gau-gau@ya.ru>
2017-09-07 20:25:01 +0700

    Add project for task 3 - tree chat

---
+++ TreeChat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeChat
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
