dde1b82a245c379cca9a2a01ed662e7a79ec42aa
GLAVAK <gau-gau@ya.ru>
2017-10-19 10:01:15 +0700

    Добавил передачу хеша аргументом командной строки

--- Md5Brute/Server/Program.cs
+++ Md5Brute/Server/Program.cs
@@ -1,17 +1,37 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-
-namespace Server
+﻿namespace Server
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            int port = int.Parse(args[0]);
+            string hashString = args[0];
+            int port = int.Parse(args[1]);
 
-            byte[] bytes = Encoding.UTF8.GetBytes("AACACACAGTCA");
-            var hash = new MD5Cng().ComputeHash(bytes);
+//            byte[] bytes = Encoding.UTF8.GetBytes("AACACACAGTCA");
+//            var hash = new MD5Cng().ComputeHash(bytes);
+
+            var hash = StringToByteArrayFastest(hashString);
+
             new Server(hash).Start(port);
+        }
+
+        public static byte[] StringToByteArrayFastest(string hex)
+        {
+            byte[] arr = new byte[hex.Length >> 1];
+
+            for (int i = 0; i < hex.Length >> 1; ++i)
+            {
+                arr[i] = (byte) ((GetHexVal(hex[i << 1]) << 4) + GetHexVal(hex[(i << 1) + 1]));
+            }
+
+            return arr;
+        }
+
+        public static int GetHexVal(char hex)
+        {
+            int val = hex;
+
+            return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
         }
     }
 }
