cfa938262d360d58e7cb111571468d6764d5fb01
GLAVAK <gau-gau@ya.ru>
2017-11-01 11:30:45 +0700

    Добавил тестовые проги, даже работают

--- ATP/Client/Program.cs
+++ ATP/Client/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Client
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using ATP;
+
+namespace Client
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            using (AtpClientSocket socket = new AtpClientSocket(new IPEndPoint(IPAddress.Loopback, 4242)))
+            {
+                var dataString = "Some test data, to send through socket";
+                byte[] data = Encoding.UTF8.GetBytes(dataString);
+                socket.Send(data, 0, data.Length);
+                Console.WriteLine($"Sent data: {dataString}");
+
+                byte[] recievedData = new byte[20];
+                socket.Recieve(recievedData, 20);
+                string recievedDataStr = Encoding.UTF8.GetString(recievedData);
+                Console.WriteLine($"Recieved data: {recievedDataStr}");
+
+                Thread.Sleep(1000);
+            }
         }
     }
 }
--- ATP/Server/Program.cs
+++ ATP/Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
+using ATP;
 
 namespace Server
 {
@@ -7,6 +8,20 @@ namespace Server
     {
         public static void Main(string[] args)
         {
+            using (AtpListenSocket listener = new AtpListenSocket(4242))
+            {
+                var socket = listener.Accept();
+
+                var dataString = "Some SERVER test data, to send through socket";
+                byte[] data = Encoding.UTF8.GetBytes(dataString);
+                socket.Send(data, 0, data.Length);
+                Console.WriteLine($"Sent data: {dataString}");
+
+                byte[] recievedData = new byte[20];
+                socket.Recieve(recievedData, 20);
+                string recievedDataStr = Encoding.UTF8.GetString(recievedData);
+                Console.WriteLine($"Recieved data: {recievedDataStr}");
+            }
         }
     }
 }
